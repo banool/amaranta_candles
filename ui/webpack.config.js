acd3846bfcc266d40f9a9c38c3c3344c5018615f
Max Koopman <m_koopman@me.com>
2020-03-22 21:01:29 -0700

    [UI] Move to explicit /static/ directory
    Also updates the webpack config to serve from the new location, and
    the build script to copy the index.html.

--- ui/webpack.config.js
+++ ui/webpack.config.js
@@ -1,35 +1,39 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: "./src/index.jsx",
   output: {
-    library: 'App',
-    filename: 'app.js',
-    path: path.resolve(__dirname, 'dist'),
-    pathinfo: true,
+    library: "App",
+    filename: "app.js",
+    path: path.resolve(__dirname, "dist/static"),
+    pathinfo: true
   },
   devtool: "eval-source-map",
   devServer: {
-    contentBase: './dist',
-    hot: true,
+    index: "index.html",
+    serveIndex: true,
+    publicPath: "/static/",
+    hot: true
   },
-  mode: 'development',
+  mode: "development",
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          plugins: ['@babel/plugin-transform-react-jsx']
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            plugins: ["@babel/plugin-transform-react-jsx"]
+          }
         }
       }
-    }]
+    ]
   },
   resolve: {
-    modules: [path.resolve(__dirname, 'node_modules')],
+    modules: [path.resolve(__dirname, "node_modules")],
     alias: {
-      'react-dom': '@hot-loader/react-dom',
+      "react-dom": "@hot-loader/react-dom"
     }
   }
 };
