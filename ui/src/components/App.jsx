c41905a85bc3ac84010152bdf2b8929063952d30
Max Koopman <m_koopman@me.com>
2020-03-22 19:54:57 -0700

    [UI] Added footer to page template

--- ui/src/components/App.jsx
+++ ui/src/components/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import Sidebar from "./Sidebar.jsx";
+import Footer from "./Footer.jsx";
 import * as colors from "../constants/colors.js";
 
 const styles = {
@@ -24,12 +25,12 @@ const styles = {
     border: "2px solid #333",
     width: 220,
     padding: 5,
-    verticalAlign: "top",
+    verticalAlign: "top"
   },
   contentCell: {
     border: "2px solid #333",
     padding: 5,
-    verticalAlign: "top",
+    verticalAlign: "top"
   }
 };
 
@@ -37,50 +38,51 @@ const App = ({}) => {
   return (
     <Router>
       <div style={styles.root}>
-        <div style={styles.content}>
-          <table style={styles.layoutTable}>
-            <tbody>
-              <tr>
-                <td style={styles.sidebarCell}>
-                  <Sidebar />
-                </td>
-                <td style={styles.contentCell}>
-                  <h2>
-                    this is the candle stuff
-                  </h2>
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the stuffs
-                  this is the rest of the stuffs this is the rest of the
-                  stuffsthis is the rest of the stuffs this is the rest of the
-                  stuffs this is the rest of the stuffs this is the rest of the
-                  stuffs this is the rest of the stuffs this is the rest of the
-                  stuffs this is the rest of the stuffs this is the rest of the
-                  stuffs this is the rest of the stuffs this is the rest of the
-                  stuffs
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          {/*
-          <Sidebar width={250} />
-          <div>
-            <Switch>
-              <Route path="/a">
-                <div>a</div>
-              </Route>
-              <Route path="/b">
-                <div>bsdfasdfsaadfgd</div>
-              </Route>
-              <Route path="/">
-                <div>home</div>
-              </Route>
-            </Switch>
+        <div>
+          <div style={styles.content}>
+            <table style={styles.layoutTable}>
+              <tbody>
+                <tr>
+                  <td style={styles.sidebarCell}>
+                    <Sidebar />
+                  </td>
+                  <td style={styles.contentCell}>
+                    {/*
+                      <Sidebar width={250} />
+                      <div>
+                        <Switch>
+                          <Route path="/a">
+                            <div>a</div>
+                          </Route>
+                          <Route path="/b">
+                            <div>bsdfasdfsaadfgd</div>
+                          </Route>
+                          <Route path="/">
+                            <div>home</div>
+                          </Route>
+                        </Switch>
+                      </div>
+                    */}
+                    <h2>this is the candle stuff</h2>
+                    this is the rest of the stuffs this is the rest of the
+                    stuffs this is the rest of the stuffs this is the rest of
+                    the stuffs this is the rest of the stuffs this is the rest
+                    of the stuffs this is the rest of the stuffs this is the
+                    rest of the stuffs this is the rest of the stuffs this is
+                    the rest of the stuffs this is the rest of the stuffs this
+                    is the rest of the stuffs this is the rest of the stuffs
+                    this is the rest of the stuffsthis is the rest of the stuffs
+                    this is the rest of the stuffs this is the rest of the
+                    stuffs this is the rest of the stuffs this is the rest of
+                    the stuffs this is the rest of the stuffs this is the rest
+                    of the stuffs this is the rest of the stuffs this is the
+                    rest of the stuffs this is the rest of the stuffs
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-        */}
+          <Footer />
         </div>
       </div>
     </Router>
