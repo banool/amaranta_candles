10f0020d77f1f58d4ea7ae465a1a7067dc98042c
Daniel Porteous <danielporteous1@gmail.com>
2020-03-23 08:20:02 -0700

    Move to model where static files are served by a separate nginx container

--- deployment/generate_service.py
+++ deployment/generate_service.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python3
 
+import argparse
+
 from jinja2 import Template
 
+parser = argparse.ArgumentParser()
+parser.add_argument("service_filename")
+parser.add_argument("--secrets-filename")
+args = parser.parse_args()
 
-BASE = "amaranta_candles.service"
-SECRETS = "secrets.env"
+args.service_filename = args.service_filename.replace(".j2", "")
 
 
-with open(f"{BASE}.j2", "r") as f:
+with open(f"{args.service_filename}.j2", "r") as f:
     template = Template(f.read())
 
-with open(SECRETS, "r") as f:
-    lines = f.read().splitlines()
+if args.secrets_filename:
+    with open(args.secrets_filename, "r") as f:
+        lines = f.read().splitlines()
 
-secrets = {}
-for l in lines:
-    k, v = l.split(" === ")
-    secrets[k] = v
+    secrets = {}
+    for l in lines:
+        k, v = l.split(" === ")
+        secrets[k] = v
+else:
+    secrets = {}
 
-with open(f"{BASE}", "w") as f:
+with open(args.service_filename, "w") as f:
     f.write(template.render(secrets=secrets))
--- server/run.sh
+++ server/run.sh
@@ -2,9 +2,6 @@
 
 PORT=$1
 
-mkdir -p distmount/
-rm distmount/dist
-ln -s dist distmount/
 python manage.py migrate --noinput
 python manage.py initadmin
 gunicorn --log-file=- --workers=2 --threads=4 --worker-class=gthread --worker-tmp-dir /dev/shm --bind 0.0.0.0:$PORT core.wsgi:application
