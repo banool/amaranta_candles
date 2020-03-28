53690ea721d47a8b76bcf4801ef7933002394f8a
Daniel Porteous <danielporteous1@gmail.com>
2020-03-28 10:17:40 -0700

    [server] Don't use trailing slash

--- server/amaranta_candles/urls.py
+++ server/amaranta_candles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from rest_framework import routers
 from . import views
 
-router = routers.SimpleRouter()
+
+router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'scent', views.ScentListCreate)
 urlpatterns = router.urls
--- server/core/settings.py
+++ server/core/settings.py
@@ -170,3 +170,4 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = 'static'
 
+APPEND_SLASH = False
