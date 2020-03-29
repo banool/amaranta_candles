1caa4a55a32566cfc9dbcd116fe5a0899ce41dba
Daniel Porteous <danielporteous1@gmail.com>
2020-03-28 17:15:31 -0700

    [server] Add test script for making a Candle

---
+++ server/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+yes | deployment_mode=dev allowed_hosts=1 secret_key=1 sql_engine='django.db.backends.sqlite3' sql_database=fake.sqlite sql_host=1 sql_user=1 sql_password=1 sql_port=1 ui_username=1 ui_email=1 ui_password=1 pipenv run python manage.py makemigrations amaranta_candles
+deployment_mode=dev allowed_hosts=2 secret_key=1 sql_engine='django.db.backends.sqlite3' sql_database=fake.sqlite sql_host=1 sql_user=1 sql_password=1 sql_port=1 ui_username=1 ui_email=1 ui_password=1 pipenv run python manage.py migrate
+deployment_mode=dev allowed_hosts=1 secret_key=1 sql_engine='django.db.backends.sqlite3' sql_database=fake.sqlite sql_host=1 sql_user=1 sql_password=1 sql_port=1 ui_username=1 ui_email=1 ui_password=1 pipenv run python manage.py shell < test.py
+rm fake.sqlite
