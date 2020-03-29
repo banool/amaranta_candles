ffa25fb289b4ff3d8e9fb051c980692b968e62fc
Daniel Porteous <danielporteous1@gmail.com>
2020-03-29 16:26:04 -0700

    [server] Functioning GraphQL get endpoint

--- server/amaranta_candles/schema.py
+++ server/amaranta_candles/schema.py
@@ -2,44 +2,137 @@ import graphene
 
 from graphene_django.types import DjangoObjectType
 
-from amaranta_candles.models import Batch, Candle, Scent, ScentCombo
+from amaranta_candles.models import Batch, Candle, Dye, DyeWithAmount, Scent, ScentCombo, ScentWithAmount, Vessel, Wax, WaxWithAmount
 
 
-def get_query(class_name_dict):
-
-    class Q:
-        pass
-
-    for klass, name in class_name_dict.items():
-
-        class T(DjangoObjectType):
-            class Meta:
-                model = klass
-                fields = "__all__"
-                name = f"{klass.__name__}Type"
-        
-        # All
-        setattr(Q, f"{name}s", graphene.List(T))
-        # Single
-        kwargs = {f"id": graphene.Int()}
-        setattr(Q, f"{name}", graphene.Field(T, **kwargs))
-
-        def f(self, info, **kwargs):
-            return klass.objects.all()
-
-        setattr(Q, f"resolve_{name}s", f)
-
-        def f(self, info, id):
-            return klass.objects.get(pk=id)
-
-        setattr(Q, f"resolve_{name}", f)
-     
-    return Q
+def get_type_class(klass):
+    class T(DjangoObjectType):
+        class Meta:
+            model = klass
+            fields = "__all__"
+            name = f"{klass.__name__}Type"
+    return T
 
 
-class_name_dict = {
-    Batch: "batch",
-}
+BatchType = get_type_class(Batch)
+CandleType = get_type_class(Candle)
+DyeType = get_type_class(Dye)
+DyeWithAmountType = get_type_class(DyeWithAmount)
+ScentType = get_type_class(Scent)
+ScentComboType = get_type_class(ScentCombo)
+ScentWithAmountType = get_type_class(ScentWithAmount)
+VesselType = get_type_class(Vessel)
+WaxType = get_type_class(Wax)
+WaxWithAmountType = get_type_class(WaxWithAmount)
 
 
-Query = get_query(class_name_dict)
+class Query:
+    batches = graphene.List(BatchType)
+    batch = graphene.Field(BatchType, id=graphene.Int())
+
+    candles = graphene.List(CandleType)
+    candle = graphene.Field(CandleType, id=graphene.Int())
+
+    dyes = graphene.List(DyeType)
+    dye = graphene.Field(DyeType, id=graphene.Int())
+
+    dyes_with_amounts = graphene.List(DyeWithAmountType)
+    dye_with_amount = graphene.Field(DyeWithAmountType, id=graphene.Int())
+
+    scents = graphene.List(ScentType)
+    scent = graphene.Field(ScentType, id=graphene.Int())
+
+    scent_combos = graphene.List(ScentComboType)
+    scent_combo = graphene.Field(ScentComboType, id=graphene.Int())
+
+    scents_with_amounts = graphene.List(ScentWithAmountType)
+    scent_with_amount = graphene.Field(ScentWithAmountType, id=graphene.Int())
+
+    vessels = graphene.List(VesselType)
+    vessel = graphene.Field(VesselType, id=graphene.Int())
+
+    waxes = graphene.List(WaxType)
+    wax = graphene.Field(WaxType, id=graphene.Int())
+
+    waxes_with_amounts = graphene.List(WaxWithAmountType)
+    wax_with_amount = graphene.Field(WaxWithAmountType, id=graphene.Int())
+
+    def resolve_batches(self, info, **kwargs):
+        return Batch.objects.all()
+
+    def resolve_batch(self, info, id=None):
+        if id is None:
+            return None
+        return Batch.objects.get(pk=id)
+
+    def resolve_candles(self, info, **kwargs):
+        return Candle.objects.all()
+
+    def resolve_candle(self, info, id=None):
+        if id is None:
+            return None
+        return Candle.objects.get(pk=id)
+
+    def resolve_dyes(self, info, **kwargs):
+        return Dye.objects.all()
+
+    def resolve_dye(self, info, id=None):
+        if id is None:
+            return None
+        return Dye.objects.get(pk=id)
+
+    def resolve_dyes_with_amounts(self, info, **kwargs):
+        return DyeWithAmount.objects.all()
+
+    def resolve_dye_with_amount(self, info, id=None):
+        if id is None:
+            return None
+        return DyeWithAmount.objects.get(pk=id)
+
+    def resolve_scents(self, info, **kwargs):
+        return Scent.objects.all()
+
+    def resolve_scent(self, info, id=None):
+        if id is None:
+            return None
+        return Scent.objects.get(pk=id)
+
+    def resolve_scent_combos(self, info, **kwargs):
+        return ScentCombo.objects.all()
+
+    def resolve_scent_combo(self, info, id=None):
+        if id is None:
+            return None
+        return ScentCombo.objects.get(pk=id)
+
+    def resolve_scents_with_amounts(self, info, **kwargs):
+        return ScentWithAmount.objects.all()
+
+    def resolve_scent_with_amount(self, info, id=None):
+        if id is None:
+            return None
+        return ScentWithAmount.objects.get(pk=id)
+
+    def resolve_vessels(self, info, **kwargs):
+        return Vessel.objects.all()
+
+    def resolve_vessel(self, info, id=None):
+        if id is None:
+            return None
+        return Vessel.objects.get(pk=id)
+
+    def resolve_waxes(self, info, **kwargs):
+        return Wax.objects.all()
+
+    def resolve_wax(self, info, id=None):
+        if id is None:
+            return None
+        return Wax.objects.get(pk=id)
+
+    def resolve_waxes_with_amounts(self, info, **kwargs):
+        return WaxWithAmount.objects.all()
+
+    def resolve_wax_with_amount(self, info, id=None):
+        if id is None:
+            return None
+        return WaxWithAmount.objects.get(pk=id)
