8315345384d30ebbae12d027629c710d42719ffd
Daniel Porteous <danielporteous1@gmail.com>
2020-05-03 09:35:02 -0700

    Complete script to load data from candle science confirmation email

--- server/amaranta_candles/management/commands/load_from_confirmation.py
+++ server/amaranta_candles/management/commands/load_from_confirmation.py
@@ -1,12 +1,18 @@
+import re
+
 from django.core.management.base import BaseCommand
-from net_worth.models import Dye, Scent, Wax, Wick
+from amaranta_candles.models import Dye, Scent, Wax, Wick
 
 
 class Command(BaseCommand):
-    help = "Loads data from candle science order confirmation"
+    help = (
+        "Loads data from candle science order confirmation. "
+        "The confirmation should just be the email copied into "
+        "a text file"
+    )
 
     def add_arguments(self, parser):
-        parser.add_argument("--order-confirmation-file")
+        parser.add_argument("--order-confirmation-file", required=True)
         parser.add_argument("--real", action="store_true")
 
     def handle(self, *args, **options):
@@ -23,28 +29,32 @@ class Command(BaseCommand):
 
             # Match Dye
             match = re.match(r'^(.*) \d+ oz Bottle.*$', l)
-            if x and "dye" in l.lower():
-                m = Dye(x[1])
+            if match and "dye" in l.lower():
+                m = Dye(name=match[1])
 
             # Match Scent
             match = re.match(r'^(.*) \d+ oz Bottle.*$', l)
-            if x and "dye" not in l.lower():
-                m = Scent(x[1])
+            if match and "dye" not in l.lower():
+                m = Scent(name=match[1])
 
             # Match Wick
             match = re.match(r'^(.*) \d+ pc Bag$', l)
-            if x:
-                m = Wick(x[1])
+            if match:
+                m = Wick(name=match[1])
 
             # Match Wax
             match = re.match(r'^(.*) \d+ lb .*$', l)
-            if x:
-                m = Wax(x[1])
+            if match:
+                m = Wax(name=match[1])
 
-            if m:
+            if m and "Free Sample" not in m.name:
                 models.append(m)
 
         for m in models:
+            existing = m.__class__.objects.filter(name=m.name)
+            if existing:
+                print(f"Not saving model because it already exists in DB: {m}")
+                continue
             if options["real"]:
                 print(f"Saving this model: {m}")
                 m.save()
